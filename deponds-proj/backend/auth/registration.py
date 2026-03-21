7441148b844b4942fc4b14f6bdb3d22bcf289a16
princeaka140 <akansoprince@gmail.com>
2026-03-22 00:04:58 +0100

    Update registration.py

--- deponds-proj/backend/auth/registration.py
+++ deponds-proj/backend/auth/registration.py
@@ -30,7 +30,7 @@ class reg():
             cursor = get_cursor(self.connect)
             cursor.execute(
                 "INSERT INTO clients (user_name, password, ip, country, name, email, reffered_by, balance) "
-                "VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
                 (username, hashed_password, IP, country, name, email, reffered_by, 200)
             )
             self.connect.commit()
