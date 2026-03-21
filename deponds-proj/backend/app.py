87e7d57570e054cd2bec443673a190b794b5f2cf
princeaka140 <akansoprince@gmail.com>
2026-03-21 17:23:05 +0100

    Update app.py

--- deponds-proj/backend/app.py
+++ deponds-proj/backend/app.py
@@ -100,7 +100,7 @@ def log_in(data: login_url):
             value = token,
             httponly = True,
             secure = True,
-            samesite = "None",
+            samesite = "Lax",
             max_age = 4000
         )
     return response
